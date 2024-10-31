62c53e72962b5d5833efe48f5e2cfec490090fae
Antoine Coulon <antoine@coulon.dev>
2024-10-31 18:05:37 +0100

    update stream ip

--- cmd/essaimkinectclient/main.go
+++ cmd/essaimkinectclient/main.go
@@ -20,7 +20,7 @@ var (
 
 func init() {
 	flag.StringVar(&addrFlag, "addr", "224.19.29.39:4242", "ip address and port used to send instructions")
-	flag.StringVar(&streamAddrFlag, "stream-addr", "224.19.29.39:4242", "ip address and port used to send instructions")
+	flag.StringVar(&streamAddrFlag, "stream-addr", "224.19.29.39:4243", "ip address and port used to send instructions")
 }
 
 func main() {
